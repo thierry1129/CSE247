650a92ba364b2f074395f0f3ec9f435149dd61ff
cytron@ACCOUNTS.AD.WUSTL.EDU <cytron@ACCOUNTS.AD.WUSTL.EDU@753c7a4d-a029-4d43-ae6e-4d0cc29fd3a0>
2016-04-19 15:05:45 +0000

    Import: from images extensions/extensions/myextension to repo terrylu at node = java/extensions/myextension

---
+++ cse247_sp16/java/extensions/myextension/given/Factorial.java
@@ -0,0 +1,10 @@
+package myextension.given;
+
+public class Factorial {
+
+	public int factorial(int n) {
+		// FIXME You complete this method
+		return 0;
+	}
+
+}
---
+++ cse247_sp16/java/extensions/myextension/solved/Factorial.java
@@ -0,0 +1,12 @@
+package myextension.solved;
+
+public class Factorial {
+
+	public int factorial(int n) {
+		if (n <= 0)
+			return 1;
+		else
+			return n * factorial(n-1);
+	}
+
+}
